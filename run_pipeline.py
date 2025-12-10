bbb14606c695dfb9e48c336fac41376b014ab744
sunnysavita10 <engsunny3365@gmail.com>
2025-12-10 21:36:13 +0530

    code foldar is updated

---
+++ run_pipeline.py
@@ -0,0 +1,4 @@
+from src.pipeline import run_pipelie
+
+if __name__== "__main__":
+    run_pipeline()
---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from src.config_loader import load_config
+from src.data_loader import load_dataset
+from src.preprocess import preprocess_data
+from src.train_xgboost import train_xgboost
+from src.train_ann import train_ann
